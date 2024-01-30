f4bf20f6689757c564c1e85b7a47f5c501f6f25f
icoo08217 <icoo08216@gmail.com>
2024-01-30 12:36:13 +0900

    첫 커밋

---
+++ src/main/java/bc/shareFleeting/ShareFleetingApplication.java
@@ -0,0 +1,13 @@
+package bc.shareFleeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareFleetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareFleetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bc/shareFleeting/ShareFleetingApplicationTests.java
@@ -0,0 +1,13 @@
+package bc.shareFleeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareFleetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
